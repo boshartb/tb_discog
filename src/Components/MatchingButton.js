e945d49139b221a2bfdab4100bc3142430f01ae7
Brant <boshartb@gmail.com>
2019-02-02 19:59:59 -0500

    changed button names

--- src/Components/MatchingButton.js
+++ src/Components/MatchingButton.js
@@ -1,4 +1,4 @@
-.import React from 'react'
+import React from 'react'
 
 const SearchButton = props =>
     <button onClick={props.onClick}>
--- src/Components/UnmatchingButton.js
+++ src/Components/UnmatchingButton.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const ClearButton = props =>
+const UnmatchingButton = props =>
     <button onClick={props.onClick}>
         Clear
   </button>
 
-export default ClearButton
+export default UnmatchingButton
