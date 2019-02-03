dda62f8e28c90be77f1264094481fea4ea992d8d
Brant <boshartb@gmail.com>
2019-02-02 20:00:49 -0500

    moved route from index to app

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,55 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Redirect, Route } from 'react-router-dom';
+
 import './ReleasePage.css';
 
+
 import Header from './Header';
-import ReleasePage from './ReleasePage;
+import NavBar from './NavBar';
+import Main from './Main';
+import Events from './Events';
+import ReleasePage from './ReleasePage';
+import SingleRelease from './SingleRelease';
+
 
 class App extends Component {
+  // app state
+  state = {
+    releaseUrl: null,
+  }
+
+  selectReleaseUrl = releaseUrl => {
+    this.setState({
+      releaseUrl: releaseUrl
+    })
+  }
+
+  selectRelease = (releaseUrl) => {
+    this.selectReleaseUrl(releaseUrl)
+    // 2. Make sure it routes
+    return (
+      <Redirect to="/SingleRelease" />
+    )
+  }
   render() {
     return (
       <div className="App">
+        <BrowserRouter>
+          <div>
+            <Header siteName="Total Black" />
+            <NavBar />
 
+            <div>
+              <Route exact path="/" component={Main} />
+              <Route path="/search/:searchTerm" component={ReleasePage} />
+              <Route path="/ReleasePage" render={(routeProps) => (<ReleasePage {...routeProps} selectRelease={this.selectRelease} />)} />
+              <Route path="/events" component={Events} />
+              <Route path="/SingleRelease" render={(routeProps) => (<SingleRelease {...routeProps} releaseUrl={this.state.releaseUrl} />)} />
+              />
+                </div>
+          </div>
+        </BrowserRouter>
+        );
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,34 +1,20 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
 import { render } from 'react-dom';
 
-import Header from './Components/Header';
-import NavBar from './Components/NavBar';
-import Main from './Components/Main';
-import Events from './Components/Events';
-import ReleasePage from './Components/ReleasePage';
-import SingleRelease from './Components/SingleRelease';
+import App from './Components/App';
 
 
 import './index.css';
 
 const Root = function () {
     return (
-        <BrowserRouter>
-            <div>
-                <Header siteName="Total Black" />
-                <NavBar />
-                <div>
-                    <Route exact path="/" component={Main} />
-                    <Route path="/search/:searchTerm" component={ReleasePage} />
-                    <Route path="/ReleasePage" component={ReleasePage} />
-                    <Route path="/events" component={Events} />
-                    <Route path="/singlerelease" component={SingleRelease} />
-                    <Route path="/releases/:releasesId/:releasesSlug" component={SingleRelease} />
-                </div>
-            </div>
-        </BrowserRouter>
+        <div>
+            <App />
+        </div>
     );
 };
 
 render(<Root />, document.querySelector('#root'));
+
+
+//single route change to release
