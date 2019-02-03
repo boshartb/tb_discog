357258207c170521e2c86d4279c23138e7b40d2e
Brant <boshartb@gmail.com>
2019-02-02 20:04:54 -0500

    refactored release to plural

--- src/Components/Release.js
+++ src/Components/Release.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
 
+// get on click
 
 class Release extends Component {
+    handleClick = () => {
+        console.log("in release")
+        this.props.selectRelease(this.props.release.resource_url)
+    }
+
+
     render() {
         return (
-            <div className="release">
+            <a className="release-box"  >
                 <h3>{this.props.release.artist}</h3>
                 <h3>{this.props.release.title}</h3>
                 <h3>{this.props.release.year}</h3>
                 <h3>{this.props.release.catno}</h3>
-            </div>
+                <button onClick={this.handleClick} >click!</button>
+
+
+            </a >
         );
     }
 }
